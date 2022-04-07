51ba89e4b5d81d118624fca48f522c0fba47a878
tomb00105 <88777684+tomb00105@users.noreply.github.com>
2022-04-07 08:50:04 +0100

    Rivers

--- Assets/Scripts/HexMesh.cs
+++ Assets/Scripts/HexMesh.cs
@@ -1,90 +1,70 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class HexMesh : MonoBehaviour {
 
-	Mesh hexMesh;
-	List<Vector3> vertices;
-	List<Color> colors;
-	List<int> triangles;
+	public bool useCollider, useColors, useUVCoordinates;
 
+	[NonSerialized] List<Vector3> vertices;
+	[NonSerialized] List<Color> colors;
+	[NonSerialized] List<Vector2> uvs;
+	[NonSerialized] List<int> triangles;
+
+	Mesh hexMesh;
 	MeshCollider meshCollider;
 
 	void Awake () {
 		GetComponent<MeshFilter>().mesh = hexMesh = new Mesh();
-		meshCollider = gameObject.AddComponent<MeshCollider>();
+		if (useCollider) {
+			meshCollider = gameObject.AddComponent<MeshCollider>();
+		}
 		hexMesh.name = "Hex Mesh";
-		vertices = new List<Vector3>();
-		colors = new List<Color>();
-		triangles = new List<int>();
 	}
 
-	public void Triangulate (HexCell[] cells) {
+	public void Clear () {
 		hexMesh.Clear();
-		vertices.Clear();
-		colors.Clear();
-		triangles.Clear();
-		for (int i = 0; i < cells.Length; i++) {
-			Triangulate(cells[i]);
+		vertices = ListPool<Vector3>.Get();
+		if (useColors) {
+			colors = ListPool<Color>.Get();
 		}
-		hexMesh.vertices = vertices.ToArray();
-		hexMesh.colors = colors.ToArray();
-		hexMesh.triangles = triangles.ToArray();
+		if (useUVCoordinates) {
+			uvs = ListPool<Vector2>.Get();
+		}
+		triangles = ListPool<int>.Get();
+	}
+
+	public void Apply () {
+		hexMesh.SetVertices(vertices);
+		ListPool<Vector3>.Add(vertices);
+		if (useColors) {
+			hexMesh.SetColors(colors);
+			ListPool<Color>.Add(colors);
+		}
+		if (useUVCoordinates) {
+			hexMesh.SetUVs(0, uvs);
+			ListPool<Vector2>.Add(uvs);
+		}
+		hexMesh.SetTriangles(triangles, 0);
+		ListPool<int>.Add(triangles);
 		hexMesh.RecalculateNormals();
-		meshCollider.sharedMesh = hexMesh;
-	}
-
-	void Triangulate(HexCell cell)
-	{
-		for (HexDirection d = HexDirection.NE; d <= HexDirection.NW; d++)
-		{
-			Triangulate(d, cell);
+		if (useCollider) {
+			meshCollider.sharedMesh = hexMesh;
 		}
 	}
 
-	void Triangulate(HexDirection direction, HexCell cell)
-	{
-		Vector3 center = cell.transform.localPosition;
-		Vector3 v1 = center + HexMetrics.GetFirstSolidCorner(direction);
-		Vector3 v2 = center + HexMetrics.GetSecondSolidCorner(direction);
-
-		AddTriangle(center, v1, v2);
-		AddTriangleColor(cell.color);
-
-        if (direction <= HexDirection.SE)
-        {
-			TriangulateConnection(direction, cell, v1, v2);
-		}
-		
+	public void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+		int vertexIndex = vertices.Count;
+		vertices.Add(HexMetrics.Perturb(v1));
+		vertices.Add(HexMetrics.Perturb(v2));
+		vertices.Add(HexMetrics.Perturb(v3));
+		triangles.Add(vertexIndex);
+		triangles.Add(vertexIndex + 1);
+		triangles.Add(vertexIndex + 2);
 	}
 
-	void TriangulateConnection(
-		HexDirection direction, HexCell cell, Vector3 v1, Vector3 v2
-	)
-	{
-		HexCell neighbor = cell.GetNeighbor(direction);
-		if (neighbor == null)
-		{
-			return;
-		}
-
-		Vector3 bridge = HexMetrics.GetBridge(direction);
-		Vector3 v3 = v1 + bridge;
-		Vector3 v4 = v2 + bridge;
-
-		AddQuad(v1, v2, v3, v4);
-		AddQuadColor(cell.color, neighbor.color);
-
-		HexCell nextNeighbor = cell.GetNeighbor(direction.Next());
-		if (direction <= HexDirection.E && nextNeighbor != null)
-		{
-			AddTriangle(v2, v4, v2 + HexMetrics.GetBridge(direction.Next()));
-			AddTriangleColor(cell.color, neighbor.color, nextNeighbor.color);
-		}
-	}
-
-	void AddTriangle (Vector3 v1, Vector3 v2, Vector3 v3) {
+	public void AddTriangleUnperturbed (Vector3 v1, Vector3 v2, Vector3 v3) {
 		int vertexIndex = vertices.Count;
 		vertices.Add(v1);
 		vertices.Add(v2);
@@ -94,26 +74,30 @@ public class HexMesh : MonoBehaviour {
 		triangles.Add(vertexIndex + 2);
 	}
 
-	void AddTriangleColor (Color color) {
+	public void AddTriangleColor (Color color) {
 		colors.Add(color);
 		colors.Add(color);
 		colors.Add(color);
 	}
 
-	void AddTriangleColor(Color c1, Color c2, Color c3)
-	{
+	public void AddTriangleColor (Color c1, Color c2, Color c3) {
 		colors.Add(c1);
 		colors.Add(c2);
 		colors.Add(c3);
 	}
 
-	void AddQuad(Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4)
-	{
+	public void AddTriangleUV (Vector2 uv1, Vector2 uv2, Vector3 uv3) {
+		uvs.Add(uv1);
+		uvs.Add(uv2);
+		uvs.Add(uv3);
+	}
+
+	public void AddQuad (Vector3 v1, Vector3 v2, Vector3 v3, Vector3 v4) {
 		int vertexIndex = vertices.Count;
-		vertices.Add(v1);
-		vertices.Add(v2);
-		vertices.Add(v3);
-		vertices.Add(v4);
+		vertices.Add(HexMetrics.Perturb(v1));
+		vertices.Add(HexMetrics.Perturb(v2));
+		vertices.Add(HexMetrics.Perturb(v3));
+		vertices.Add(HexMetrics.Perturb(v4));
 		triangles.Add(vertexIndex);
 		triangles.Add(vertexIndex + 2);
 		triangles.Add(vertexIndex + 1);
@@ -122,19 +106,38 @@ public class HexMesh : MonoBehaviour {
 		triangles.Add(vertexIndex + 3);
 	}
 
-	void AddQuadColor(Color c1, Color c2, Color c3, Color c4)
-	{
+	public void AddQuadColor (Color color) {
+		colors.Add(color);
+		colors.Add(color);
+		colors.Add(color);
+		colors.Add(color);
+	}
+
+	public void AddQuadColor (Color c1, Color c2) {
+		colors.Add(c1);
+		colors.Add(c1);
+		colors.Add(c2);
+		colors.Add(c2);
+	}
+
+	public void AddQuadColor (Color c1, Color c2, Color c3, Color c4) {
 		colors.Add(c1);
 		colors.Add(c2);
 		colors.Add(c3);
 		colors.Add(c4);
 	}
 
-	void AddQuadColor(Color c1, Color c2)
-	{
-		colors.Add(c1);
-		colors.Add(c1);
-		colors.Add(c2);
-		colors.Add(c2);
+	public void AddQuadUV (Vector2 uv1, Vector2 uv2, Vector3 uv3, Vector3 uv4) {
+		uvs.Add(uv1);
+		uvs.Add(uv2);
+		uvs.Add(uv3);
+		uvs.Add(uv4);
+	}
+
+	public void AddQuadUV (float uMin, float uMax, float vMin, float vMax) {
+		uvs.Add(new Vector2(uMin, vMin));
+		uvs.Add(new Vector2(uMax, vMin));
+		uvs.Add(new Vector2(uMin, vMax));
+		uvs.Add(new Vector2(uMax, vMax));
 	}
 }
